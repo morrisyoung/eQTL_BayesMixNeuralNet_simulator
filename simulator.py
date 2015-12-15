0b71aa39e30a882bced48595da640748c5a60a1e
morrisyoung <sdmorrisys@gmail.com>
2015-12-15 16:53:07 -0500

    update

--- simulator.py
+++ simulator.py
@@ -3,12 +3,11 @@
 ## function: generate the coefficients according to the specified graphical model (the neural model in a Bayesian approach), and generate the corresponding expression level for ALL genes
 ## notes:
 ##	1. we should simulate tissue specificity, as the modeling takes consideration of that;
-##	2. xxx
+##	2. when we say a factor, it's latent factor condensed from some original variables
 ##	3. xxx
 
 
 import numpy as np
-
 
 
 ##==== global variables
@@ -16,6 +15,9 @@ import numpy as np
 ##	1. shall we simulate Spike and Slab?
 ##	2. xxx
 ##=====================
+#==== chromosome
+L = 0								# unit: basepair
+
 #==== individual
 n_individual = 0
 
@@ -42,15 +44,14 @@ factor_cell_beta_rep = {cell factor:[], ...}			# coefficient lists for cell fact
 beta_factor_cell_rep = {tissue:{gene:[], ...}, ...}		# coefficient lists of cell factors for genes in tissues
 
 #==== factor_batch (analogous to cell factor pathway)
-n_factor_batch = 0						# n_factor_batch = n_factor_batch_individual + n_factor_batch_sample
-n_factor_batch_individual = 0
-n_factor_batch_sample = 0
+n_batch = 0							# n_batch = n_batch_individual + n_batch_sample
+n_batch_individual = 0
+n_batch_sample = 0
+#n_factor_batch							# the number of latent batch factors
 batch_individual_rep = {individual:[], ...}			# batch variable lists for individuals
 batch_tissue_sample_rep = {individual:{tissue:[], ...}, ...}	# batch variable lists for tissue samples in individuals
 factor_batch_beta_rep = {batch factor:[], ...}			# coefficient lists for batch factors
-beta_factor_batch_rep = {tissue:[], ...}			# coefficient lists of batch factors for genes
-
-
+beta_factor_batch_rep = {gene:[], ...}				# coefficient lists of batch factors for genes
 
 
 
@@ -61,7 +62,10 @@ beta_factor_batch_rep = {tissue:[], ...}			# coefficient lists of batch factors 
 ##	1. simulate variables observed and latent excluding those along the generation process
 ##	2. xxx
 ##=====================
-def simu_geno():	# variables and beta (cis-)
+def simu_genotype():	# variables
+	# to fill in: 
+	##SNP_rep = {}	# {individual:[], xxx:[], ...}			# real value lists, in [0,1], for individuals
+	##SNP_pos_list = []
 
 """
 ## function: randomly generate the genotypes (minor allele frequency) of n individuals for n_SNP independent sites, value in [0, 0.5, 1]
@@ -81,23 +85,46 @@ n_SNP = 10000
 
 	file.close()
 """
+
 	return
+
+def simu_genotype_beta():	# beta (cis-); one more coefficient as the constant item in linear regression
+	# to fill in:
+	##SNP_beta_rep = {tissue:{gene:[], ...}, ...}			# cis- SNP beta lists for different genes in tissuess
+	# later on, after "SNP gene pos map"
+
+
+
+	return
+
+
+def simu_gene():
+	# to fill in:
+	##gene_pos_list = []
+
+
+	return
+
+
+
+def simu_cell_factor():
+	# to fill in:
+	##factor_cell_beta_rep = {cell factor:[], ...}			# coefficient lists for cell factors
+	##beta_factor_cell_rep = {tissue:{gene:[], ...}, ...}		# coefficient lists of cell factors for genes in tissues
+
+
+	return
+
 
 
 ## simulate batch variables for all individuals, and for all samples in different individuals; then concatenate them when using them
-def simu_batch():	# variables and beta
+def simu_batch_factor():	# variables and beta
+	# to fill in:
+	##batch_individual_rep = {individual:[], ...}			# batch variable lists for individuals
+	##batch_tissue_sample_rep = {individual:{tissue:[], ...}, ...}	# batch variable lists for tissue samples in individuals
+	##factor_batch_beta_rep = {batch factor:[], ...}			# coefficient lists for batch factors
+	##beta_factor_batch_rep = {gene:[], ...}
 
-	return
-
-def simu_cell_beta():
-
-	return
-
-def simu_beta_cell_fac():
-
-	return
-
-def simu_beta_batch():
 
 	return
 
@@ -106,6 +133,18 @@ def simu_beta_batch():
 
 
 
+
+
+
+
+
+##==== other utilities
+## notes: TODO
+##	1. xxx
+##=====================
+def SNP_gene_map():
+	# to fill in:
+	##pos_map = {}							# {gene:[snp1, snp2], ...}
 
 
 
@@ -114,6 +153,165 @@ def simu_beta_batch():
 
 
 if __name__ == '__main__':
+
+
+	##===========================
+	##==== initialize dimensions
+	##===========================
+	#==== chromosome
+	L = 100000000				# 100MB
+	#==== individual
+	n_individual = 185			# as in GTEx.v.4
+	#==== tissue
+	n_tissue = 17				# as in GTEx.v.4
+	#==== SNP
+	n_SNP = 100000				# 100K, so SNP density is 1/1,000bp
+	#==== gene
+	n_gene = 2000				# 2K, so gene density is 1/50,000bp
+	#==== factor_cell
+	n_factor_cell = 400			# as evaluated empirically
+	#==== factor_batch (analogous to cell factor pathway)
+	n_batch = 229
+	n_batch_individual = 160		# as in GTEx.v.4
+	n_batch_sample = 69			# as in GTEx.v.4
+	n_factor_batch = 400			# as evaluated empirically, the same with cell factors
+
+
+
+
+
+	##=======================================================
+	##==== simulate variables (fill in the dimensions first)
+	##=======================================================
+	#==== chromosome
+	#L = 100000000				# 100MB
+	#==== individual
+	#n_individual = 185			# as in GTEx.v.4
+	#==== tissue
+	#n_tissue = 17				# as in GTEx.v.4
+
+
+	#==== SNP
+	#n_SNP = 100000				# 100K, so SNP density is 1/1,000bp
+	##SNP_rep = {}	# {individual:[], xxx:[], ...}			# real value lists, in [0,1], for individuals
+	for i in range(n_individual):
+		SNP_rep[i] = []
+		for j in range(n_SNP):
+			SNP_rep[i].append(0)
+	##SNP_pos_list = []
+	for i in range(n_SNP):
+		SNP_pos_list.append(0)
+	##SNP_beta_rep = {tissue:{gene:[], ...}, ...}			# cis- SNP beta lists for different genes in tissuess
+	# later on, after "SNP gene pos map"
+	simu_genotype()
+
+
+	#==== gene
+	#n_gene = 2000				# 2K, so gene density is 1/50,000bp
+	##gene_rep = {individual:{tissue:[], ...}, ...}			# gene expression list for tissue samples for individuals
+	for i in range(n_individual):
+		gene_rep[i] = {}
+		for j in range(n_tissue):
+			gene_rep[i][j] = []
+			for k in range(n_gene):
+				gene_rep[i][j].append(0)
+	##gene_pos_list = []
+	simu_gene()
+
+
+	#==== SNP gene pos map
+	#pos_map = {}							# {gene:[snp1, snp2], ...}
+	SNP_gene_map()
+	##SNP_beta_rep = {tissue:{gene:[], ...}, ...}			# cis- SNP beta lists for different genes in tissuess
+	simu_genotype_beta()
+
+
+	#==== factor_cell
+	#n_factor_cell = 400			# as evaluated empirically
+	##factor_cell_beta_rep = {cell factor:[], ...}			# coefficient lists for cell factors
+	for i in range(n_factor_cell):
+		factor_cell_beta_rep[i] = []
+		for j in range(n_SNP):
+			factor_cell_beta_rep[i].append(0)
+		factor_cell_beta_rep[i].append(0)
+	#beta_factor_cell_rep = {tissue:{gene:[], ...}, ...}		# coefficient lists of cell factors for genes in tissues
+	for i in range(n_tissue):
+		beta_factor_cell_rep[i] = {}
+		for j in range(n_gene):
+			beta_factor_cell_rep[i][j] = []
+			for k in range(n_factor_cell):
+				beta_factor_cell_rep[i][j].append(0)
+			beta_factor_cell_rep[i][j].append(0)	# the constant item
+	simu_cell_factor()
+
+
+	#==== factor_batch (analogous to cell factor pathway)
+	#n_batch = 229
+	#n_batch_individual = 160		# as in GTEx.v.4
+	#n_batch_sample = 69			# as in GTEx.v.4
+	#n_factor_batch = 400
+	##batch_individual_rep = {individual:[], ...}			# batch variable lists for individuals
+	for i in range(n_individual):
+		batch_individual_rep[i] = []
+		for j in range(n_batch_individual):
+			batch_individual_rep[i].append(0)
+	##batch_tissue_sample_rep = {individual:{tissue:[], ...}, ...}	# batch variable lists for tissue samples in individuals
+	for i in range(n_individual):
+		batch_tissue_sample_rep[i] = {}
+		for j in range(n_tissue):
+			batch_tissue_sample_rep[i][j] = []
+			for k in range(n_batch_sample):
+				batch_tissue_sample_rep[i][j].append(0)
+	##factor_batch_beta_rep = {batch factor:[], ...}			# coefficient lists for batch factors
+	for i in range(n_factor_batch):
+		factor_batch_beta_rep[i] = []
+		for j in range(n_batch):
+			factor_batch_beta_rep[i].append(0)2
+		factor_batch_beta_rep[i].append(0)
+	##beta_factor_batch_rep = {gene:[], ...}
+	for i in range(n_gene):
+		beta_factor_batch_rep[i] = []
+		for j in range(n_factor_batch):
+			beta_factor_batch_rep[i].append(0)
+		beta_factor_batch_rep[i].append(0)
+	simu_batch_factor()
+
+
+
+
+
+
+
+
+
+
+
+
+	##=======================================================
+	##==== generate the expression profile through the model
+	##=======================================================
+	##gene_rep = {individual:{tissue:[], ...}, ...}			# gene expression list for tissue samples for individuals
+
+
+
+
+
+
+
+
+
+	##======================
+	##==== parameter saving
+	##======================
+
+
+
+
+
+
+
+
+
 
 
 
@@ -165,8 +363,4 @@ if __name__ == '__main__':
 
 
 
-
-
-	###
-	
 
